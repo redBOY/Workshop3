4a4d4fcccffe04c1a00d60827db384a73f2d2a09
“Adithya <adithyasrinivas66@gmail.com>
2022-09-05 10:50:55 +0530

    initial commit

---
+++ Deck.java
@@ -0,0 +1,28 @@
+package com.bridgelabzWorkshop3;
+
+public class Deck {
+    public static void main(String[] args) {
+        String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] rank = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+        String[][] cards = new String[4][13];
+        public void initializeCards() {
+            for (int i = 0; i < 4; i++) {
+                for (int j = 0; j < 13; j++) {
+                    cards[i][j] = suit[i] + ' ' + rank[j];
+                }
+            }
+            System.out.println("Number of cards in deck");
+        }
+        public void printCards () {
+            for (int i = 0; i < suit.length; i++) {
+                for (j = 0; j < ranks.length; j++) {
+                    System.out.println(deck[i][j]);
+                }
+            }
+        }
+        public static void main(String[] args){
+            Deck abc = new Deck();
+            abc.initializeCards();
+            abc.printCards();
+        }
+    }
